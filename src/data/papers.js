bb794808a2d1eecc9cb422729c610bea30a5b1d2
antoniorv6 <ariosvila@gmail.com>
2024-10-04 22:31:15 +0200

    feat(landing): Landing page finished

---
+++ src/data/papers.js
@@ -0,0 +1,27 @@
+const papers = [
+    {
+      title: "End-to-End Full-Page Optical Music Recognition for Pianoform Sheet Music",
+      authors: "Antonio Ríos-Vila, Jorge Calvo-Zaragoza, David Rizo, Thierry Paquet",
+      year: "2024",
+      venue: "ArXiv∫"
+    },
+    {
+      title: "Sheet Music Transformer: End-To-End Optical Music Recognition Beyond Monophonic Transcription",
+      authors: "Antonio Ríos-Vila, Jorge Calvo-Zaragoza, Thierry Paquet",
+      year: "2024",
+      venue: "ICDAR"
+    },
+    {
+      title: "A Transformer Approach for Polyphonic Audio-to-Score Transcription.",
+      authors: "María Alfaro-Contreras, Antonio Ríos-Vila, José J. Valero, Jorge Calvo-Zaragoza",
+      year: "2024",
+      venue: "ICASSP"
+    },
+    {
+      title: "End-to-end optical music recognition for pianoform sheet music.",
+      authors: "Antonio Ríos-Vila, David Rizo, Jorge Calvo-Zaragoza, José M. Iñesta",
+      year: "2024",
+      venue: "IJDAR"
+    }]
+
+export default papers;
---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+const projects = [
+    {
+        title: 'Sheet Music Transformer',
+        description: 'End-to-end full-page optical music recognition for pianoform sheet music',
+        language: 'Transformers',
+        tags: ['OMR', 'Python', 'Pianoform'],
+        icon: 'Music'
+    },
+    {
+        title: 'Rotations are all you need',
+        description: 'A generic end-to-end OMR transcription system',
+        language: 'Python',
+        tags: ['OMR', 'Pianoform', 'Monophony', 'Lyrics'],
+        icon: 'Music'
+    },
+    {
+        title: 'YALE',
+        description: '[WIP] Yet Another LaTex Editor',
+        language: 'Tauri',
+        tags: ['Desktop app', 'LaTex'],
+        icon: 'Laptop'
+    },
+    {
+        title: 'ReadSco',
+        description: 'An online tool to transcribe your music scores',
+        language: 'Angular',
+        tags: ['OMR', 'Application'],
+        icon: 'Globe'
+    },
+]
+
+export default projects;
