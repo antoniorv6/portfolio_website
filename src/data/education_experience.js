4bd43f700532feef157f7e2945021ed729972008
antoniorv6 <ariosvila@gmail.com>
2024-10-05 11:16:55 +0200

    feat(papers): Created papers page and fetched data from DBLP

---
+++ src/data/education_experience.js
@@ -0,0 +1,47 @@
+const education = [
+    {
+        date: "July 2024",
+        title: "Ph.D in Computer Science - University of Alicante",
+        description: "Obtained the Ph.D title in Computer Science with the thesis: \" End-to-end full-page optical music recognition beyond staff-level transcription\".",
+    },
+    {
+        date: "June 2021",
+        title: "Data Science Master",
+        description: "Master in Data Science at the University of Alicante. Best class expedient award received.",
+    },
+    {
+        date: "June 2020",
+        title: "Multimedia Engineering Degree",
+        description: "Graduated in Multimedia Engineering at the University of Alicante.",
+    },
+]
+
+const works = [
+    {
+        date: "Oct 2021 - July 2024",
+        title: "Ph.D Candidate - Generalitat Valenciana",
+        description: "Ph.D Candidate with the grant ACIF 2021/036 from the Generalitat Valenciana. Working on my Ph.D thesis: \" End-to-end full-page optical music recognition beyond staff-level transcritption\".",
+    },
+    {
+        date: "Oct 2019 - March 2021",
+        title: "Full-Stack Developer - University of Alicante",
+        description: "Full Stack development of HISPAMUS MuRET API for music scores research and recognition. Design and programming of Deep Learning Systems for advanced API functionalities",
+    },
+    {
+        date: "January 2019 - June 2019",
+        title: "Full-Stack Developer - University of Alicante",
+        description: "API and database programming for Metapress research project at the University of Alicante",
+    },
+    {
+        date: "January 2019 - June 2019",
+        title: "Full-Stack Developer - University of Alicante",
+        description: "API and database programming for Metapress research project at the University of Alicante",
+    },
+    {
+        date: "March 2018 - January 2019",
+        title: "Full-Stack Developer - Complutense University of Madrid",
+        description: "Web application programming for the TEU research project developed with Laravel and Bootstrap. Database programming and design in PostgreSQL.",
+    },
+]
+
+export { education, works };
--- src/data/papers.js
+++ src/data/papers.js
@@ -1,27 +1,236 @@
 const papers = [
-    {
-      title: "End-to-End Full-Page Optical Music Recognition for Pianoform Sheet Music",
-      authors: "Antonio Ríos-Vila, Jorge Calvo-Zaragoza, David Rizo, Thierry Paquet",
-      year: "2024",
-      venue: "ArXiv∫"
-    },
-    {
-      title: "Sheet Music Transformer: End-To-End Optical Music Recognition Beyond Monophonic Transcription",
-      authors: "Antonio Ríos-Vila, Jorge Calvo-Zaragoza, Thierry Paquet",
-      year: "2024",
-      venue: "ICDAR"
-    },
-    {
-      title: "A Transformer Approach for Polyphonic Audio-to-Score Transcription.",
-      authors: "María Alfaro-Contreras, Antonio Ríos-Vila, José J. Valero, Jorge Calvo-Zaragoza",
-      year: "2024",
-      venue: "ICASSP"
-    },
-    {
-      title: "End-to-end optical music recognition for pianoform sheet music.",
-      authors: "Antonio Ríos-Vila, David Rizo, Jorge Calvo-Zaragoza, José M. Iñesta",
-      year: "2024",
-      venue: "IJDAR"
-    }]
+  {
+    "author": "María Alfaro-Contreras, Antonio Ríos-Vila, Jose J. Valero-Mas, Jorge Calvo-Zaragoza",
+    "title": "A Transformer Approach for Polyphonic Audio-to-Score Transcription.",
+    "pages": "706-710",
+    "year": 2024,
+    "venue": "ICASSP",
+    "ee": "https://doi.org/10.1109/ICASSP48485.2024.10447162",
+    "url": "db/conf/icassp/icassp2024.html#Alfaro-Contreras24",
+    "venue_type": "congress"
+  },
+  {
+    "author": "Antonio Ríos-Vila, Jorge Calvo-Zaragoza, Thierry Paquet",
+    "title": "Sheet Music Transformer: End-To-End Optical Music Recognition Beyond Monophonic Transcription.",
+    "pages": "20-37",
+    "year": 2024,
+    "venue": "ICDAR",
+    "ee": "https://doi.org/10.1007/978-3-031-70552-6_2",
+    "url": "db/conf/icdar/icdar2024-6.html#RiosVilaCP24",
+    "stream": "streams/conf/icdar",
+    "venue_type": "congress"
+  },
+  {
+    "author": "Antonio Ríos-Vila, Jorge Calvo-Zaragoza, David Rizo, Thierry Paquet",
+    "title": "Sheet Music Transformer ++: End-to-End Full-Page Optical Music Recognition for Pianoform Sheet Music.",
+    "year": 2024,
+    "volume": "abs/2405.12105",
+    "venue": "ArXiv",
+    "ee": "https://doi.org/10.48550/arXiv.2405.12105",
+    "url": "db/journals/ArXiv/ArXiv2405.html#abs-2405-12105",
+    "venue_type": "journal"
+  },
+  {
+    "author": "Antonio Ríos-Vila, David Rizo, José M. Iñesta, Jorge Calvo-Zaragoza",
+    "title": "End-to-end optical music recognition for pianoform sheet music.",
+    "pages": "347-362",
+    "year": 2023,
+    "month": "September",
+    "volume": 26,
+    "venue": "Int. J. Document Anal. Recognit.",
+    "number": 3,
+    "ee": "https://doi.org/10.1007/s10032-023-00432-z",
+    "url": "db/journals/ijdar/ijdar26.html#RiosVilaRIC23",
+    "venue_type": "journal"
+  },
+  {
+    "author": "Enrique Vidal 0001, Alejandro H. Toselli, Antonio Ríos-Vila, Jorge Calvo-Zaragoza",
+    "title": "End-to-End page-Level assessment of handwritten text recognition.",
+    "pages": 109695,
+    "year": 2023,
+    "month": "October",
+    "volume": 142,
+    "venue": "Pattern Recognit.",
+    "ee": "https://doi.org/10.1016/j.patcog.2023.109695",
+    "url": "db/journals/pr/pr142.html#VidalTRC23",
+    "venue_type": "journal"
+  },
+  {
+    "author": "María Alfaro-Contreras, Antonio Ríos-Vila, Jose J. Valero-Mas, Jorge Calvo-Zaragoza",
+    "title": "Few-shot symbol classification via self-supervised learning and nearest neighbor.",
+    "pages": "1-8",
+    "year": 2023,
+    "month": "March",
+    "volume": 167,
+    "venue": "Pattern Recognit. Lett.",
+    "ee": "https://doi.org/10.1016/j.patrec.2023.01.014",
+    "url": "db/journals/prl/prl167.html#AlfaroContrerasRVC23",
+    "venue_type": "journal"
+  },
+  {
+    "author": "Jorge Calvo-Zaragoza, Juan C. Martinez-Sevilla, Carlos Peñarrubia, Antonio Ríos-Vila",
+    "title": "Optical Music Recognition: Recent Advances, Current Challenges, and Future Directions.",
+    "pages": "94-104",
+    "year": 2023,
+    "venue": "ICDAR Workshops",
+    "ee": "https://doi.org/10.1007/978-3-031-41498-5_7",
+    "url": "db/conf/icdar/icdar2023w-1.html#CalvoZaragozaMPR23",
+    "venue_type": "congress"
+  },
+  {
+    "author": "Juan C. Martinez-Sevilla, Antonio Ríos-Vila, Francisco J. Castellanos 0001, Jorge Calvo-Zaragoza",
+    "title": "A Holistic Approach for Aligned Music and Lyrics Transcription.",
+    "pages": "185-201",
+    "year": 2023,
+    "venue": "ICDAR",
+    "ee": "https://doi.org/10.1007/978-3-031-41676-7_11",
+    "url": "db/conf/icdar/icdar2023-1.html#MartinezSevillaRCC23",
+    "venue_type": "congress"
+  },
+  {
+    "author": "Enrique Vidal 0001, Alejandro H. Toselli, Antonio Ríos-Vila, Jorge Calvo-Zaragoza",
+    "title": "End-to-End Page-Level Assessment of Handwritten Text Recognition.",
+    "year": 2023,
+    "volume": "abs/2301.05935",
+    "venue": "ArXiv",
+    "ee": "https://doi.org/10.48550/arXiv.2301.05935",
+    "url": "db/journals/ArXiv/ArXiv2301.html#abs-2301-05935",
+    "venue_type": "journal"
+  },
+  {
+    "author": "Enrique Mas-Candela, Antonio Ríos-Vila, Jorge Calvo-Zaragoza",
+    "title": "Image Transformation Sequence Retrieval with General Reinforcement Learning.",
+    "year": 2023,
+    "volume": "abs/2307.06630",
+    "venue": "ArXiv",
+    "ee": "https://doi.org/10.48550/arXiv.2307.06630",
+    "url": "db/journals/ArXiv/ArXiv2307.html#abs-2307-06630",
+    "venue_type": "journal"
+  },
+  {
+    "author": "Francisco J. Castellanos 0001, Carlos Garrido-Munoz, Antonio Ríos-Vila, Jorge Calvo-Zaragoza",
+    "title": "Region-based layout analysis of music score images.",
+    "pages": 118211,
+    "year": 2022,
+    "volume": 209,
+    "venue": "Expert Syst. Appl.",
+    "ee": "https://doi.org/10.1016/j.eswa.2022.118211",
+    "url": "db/journals/eswa/eswa209.html#CastellanosGRC22",
+    "venue_type": "journal"
+  },
+  {
+    "author": "Carlos Garrido-Munoz, Antonio Ríos-Vila, Jorge Calvo-Zaragoza",
+    "title": "A holistic approach for image-to-graph: application to optical music recognition.",
+    "pages": "293-303",
+    "year": 2022,
+    "volume": 25,
+    "venue": "Int. J. Document Anal. Recognit.",
+    "number": 4,
+    "ee": "https://doi.org/10.1007/s10032-022-00417-4",
+    "url": "db/journals/ijdar/ijdar25.html#Garrido-MunozRC22",
+    "venue_type": "journal"
+  },
+  {
+    "author": "María Alfaro-Contreras, Antonio Ríos-Vila, Jose J. Valero-Mas, José M. Iñesta, Jorge Calvo-Zaragoza",
+    "title": "Decoupling music notation to improve end-to-end Optical Music Recognition.",
+    "pages": "157-163",
+    "year": 2022,
+    "volume": 158,
+    "venue": "Pattern Recognit. Lett.",
+    "ee": "https://doi.org/10.1016/j.patrec.2022.04.032",
+    "url": "db/journals/prl/prl158.html#Alfaro-Contreras22",
+    "venue_type": "journal"
+  },
+  {
+    "author": "Enrique Mas-Candela, Antonio Ríos-Vila, Jorge Calvo-Zaragoza",
+    "title": "A First Approach to Image Transformation Sequence Retrieval.",
+    "pages": "321-332",
+    "year": 2022,
+    "venue": "IbPRIA",
+    "ee": "https://doi.org/10.1007/978-3-031-04881-4_26",
+    "url": "db/conf/ibpria/ibpria2022.html#Mas-CandelaRC22",
+    "venue_type": "congress"
+  },
+  {
+    "author": "Antonio Ríos-Vila, José M. Iñesta, Jorge Calvo-Zaragoza",
+    "title": "On the Use of Transformers for End-to-End Optical Music Recognition.",
+    "pages": "470-481",
+    "year": 2022,
+    "venue": "IbPRIA",
+    "ee": "https://doi.org/10.1007/978-3-031-04881-4_37",
+    "url": "db/conf/ibpria/ibpria2022.html#Rios-VilaIC22",
+    "venue_type": "congress"
+  },
+  {
+    "author": "Carlos Garrido-Munoz, Antonio Ríos-Vila, Jorge Calvo-Zaragoza",
+    "title": "Retrieval of Music-Notation Primitives via Image-to-Sequence Approaches.",
+    "pages": "482-492",
+    "year": 2022,
+    "venue": "IbPRIA",
+    "ee": "https://doi.org/10.1007/978-3-031-04881-4_38",
+    "url": "db/conf/ibpria/ibpria2022.html#Garrido-MunozRC22",
+    "venue_type": "congress"
+  },
+  {
+    "author": "Antonio Ríos-Vila, María Alfaro-Contreras, Jose J. Valero-Mas, Jorge Calvo-Zaragoza",
+    "title": "Few-Shot Music Symbol Classification via Self-Supervised Learning and Nearest Neighbor.",
+    "pages": "93-107",
+    "year": 2022,
+    "venue": "ICPR Workshops",
+    "ee": "https://doi.org/10.1007/978-3-031-37731-0_8",
+    "url": "db/conf/icpr/icpr2022w3.html#RiosVilaAVC22",
+    "venue_type": "congress"
+  },
+  {
+    "author": "Antonio Ríos-Vila, José M. Iñesta, Jorge Calvo-Zaragoza",
+    "title": "End-to-End Full-Page Optical Music Recognition for Mensural Notation.",
+    "pages": "226-232",
+    "year": 2022,
+    "venue": "ISMIR",
+    "ee": "https://archives.ismir.net/ismir2022/paper/000026.pdf",
+    "url": "db/conf/ismir/ismir2022.html#Rios-VilaIC22",
+    "venue_type": "congress"
+  },
+  {
+    "author": "Francisco J. Castellanos 0001, Carlos Garrido-Munoz, Antonio Ríos-Vila, Jorge Calvo-Zaragoza",
+    "title": "Region-based Layout Analysis of Music Score Images.",
+    "year": 2022,
+    "volume": "abs/2201.04214",
+    "venue": "ArXiv",
+    "ee": "https://arxiv.org/abs/2201.04214",
+    "url": "db/journals/ArXiv/ArXiv2201.html#abs-2201-04214",
+    "venue_type": "journal"
+  },
+  {
+    "author": "Antonio Ríos-Vila, David Rizo, Jorge Calvo-Zaragoza",
+    "title": "Complete Optical Music Recognition via Agnostic Transcription and Machine Translation.",
+    "pages": "661-675",
+    "year": 2021,
+    "venue": "ICDAR",
+    "ee": "https://doi.org/10.1007/978-3-030-86334-0_43",
+    "url": "db/conf/icdar/icdar2021-3.html#Rios-VilaRC21",
+    "venue_type": "congress"
+  },
+  {
+    "author": "Antonio Ríos-Vila, Jorge Calvo-Zaragoza, David Rizo",
+    "title": "Evaluating Simultaneous Recognition and Encoding for Optical Music Recognition.",
+    "pages": "10-17",
+    "year": 2020,
+    "venue": "DLfM",
+    "ee": "https://doi.org/10.1145/3424911.3425512",
+    "url": "db/conf/dlfm/dlfm2020.html#Rios-VilaCR20",
+    "venue_type": "congress"
+  },
+  {
+    "author": "Antonio Ríos-Vila, Jorge Calvo-Zaragoza, José M. Iñesta",
+    "title": "Exploring the two-dimensional nature of music notation for score recognition with end-to-end approaches.",
+    "pages": "193-198",
+    "year": 2020,
+    "venue": "ICFHR",
+    "ee": "https://doi.org/10.1109/ICFHR2020.2020.00044",
+    "url": "db/conf/icfhr/icfhr2020.html#Rios-VilaCI20",
+    "venue_type": "congress"
+  }
+]
 
 export default papers;
